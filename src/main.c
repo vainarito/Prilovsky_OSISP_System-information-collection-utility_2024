cc570d05de82c8e4e5c5721aec3d2831530bfc96
Victor Prilovsky <112697876+vainarito@users.noreply.github.com>
2024-06-05 14:29:34 +0300

    Add files via upload

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include "mainMenu.h"
+
+int main(int argc, char **argv)
+{
+    GtkApplication *app;
+    int status;
+    // инициализация приложения
+    app = gtk_application_new(NULL, G_APPLICATION_FLAGS_NONE);
+    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+    // Запуск приложениz
+    status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
+
+    return status;
+}
